bdbcedb21b0b3209e515c0d5bd742eceff5a75ff
bcepnez <senju_mr@hotmail.com>
2018-07-10 17:16:52 +0700

    first commit

---
+++ app/src/main/java/com/example/benzrst/testapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.benzrst.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity {
+    ServerSocket ss;
+    Button send ;
+    TextView message,reply;
+    EditText editText;
+    String text="";
+    boolean status=true;
+
+    Thread myThread;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        message = (TextView)findViewById(R.id.OreNoMessage);
+        reply = (TextView)findViewById(R.id.Chat);
+        editText = (EditText)findViewById(R.id.editText);
+        send = (Button)findViewById(R.id.sender);
+        myThread = new Thread(new MyServerThread());
+        myThread.start();
+    }
+
+//    Android Sender
+    public void sender(View v){
+//        status = false;
+        myThread.interrupt();
+        MessageSender messageSender = new MessageSender();
+        text=editText.getText().toString();
+        messageSender.execute(text);
+        message.setText(text);
+        editText.setText("");
+//        status = true;
+//        myThread.start();
+    }
+//  Android Reciever
+    class MyServerThread implements Runnable {
+        Socket s;
+        InputStreamReader isr;
+        BufferedReader br;
+        String message;
+        Handler h = new Handler();
+        @Override
+        public void run() {
+            try {
+                ss = new ServerSocket(8000);
+//                while (true){
+                while (status){
+                    s = ss.accept();
+                    isr = new InputStreamReader(s.getInputStream());
+                    br = new BufferedReader(isr);
+                    message = br.readLine();
+                    Log.d("Message",message);
+                    h.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            reply.setText(message);
+                        }
+                    });
+                    s.close();
+                }
+                ss.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
